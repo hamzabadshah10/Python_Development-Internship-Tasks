b28a5a04668e8f7ef723566ecfacda53c994cc63
Hamza Badshah <154834796+hamzabadshah10@users.noreply.github.com>
2025-06-27 04:16:24 -0700

    Add files via upload

---
+++ task2_intro_script.py
@@ -0,0 +1,9 @@
+#Task 02:
+#Write a python program to print name, hobby, age, and why you choose python
+
+name = "Hamza Badshah"
+hobby = "Programming"
+age = 21
+why_python = "To develop logical thinking and problem solving skills"
+
+print ("\n name:",name,"\n hobby:",hobby,"\n age:", age,"\n goal:",why_python)
