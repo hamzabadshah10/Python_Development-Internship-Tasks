3d761aa1369aafdb8904e76f5e7f6fa4e9dd6bdb
Hamza Badshah <154834796+hamzabadshah10@users.noreply.github.com>
2025-07-01 04:41:04 -0700

    Delete task2_intro_script.py

--- task2_intro_script.py
+++
@@ -1,9 +0,0 @@
-#Task 02:
-#Write a python program to print name, hobby, age, and why you choose python
-
-name = "Hamza Badshah"
-hobby = "Programming"
-age = 21
-why_python = "To develop logical thinking and problem solving skills"
-
-print ("\n name:",name,"\n hobby:",hobby,"\n age:", age,"\n goal:",why_python)
