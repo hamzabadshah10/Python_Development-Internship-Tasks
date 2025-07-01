fb5bbbd8799a28f66dcec8d3eddc9f190484ee6a
Hamza Badshah <154834796+hamzabadshah10@users.noreply.github.com>
2025-07-01 04:45:13 -0700

    Add files via upload

---
+++ week_01-tasks/task3_addition.py
@@ -0,0 +1,8 @@
+# write a python program to ask user to enter the two numbers, also show the result of two numbers.
+
+num1 = int (input ("Enter First Number:"))
+num2 = int (input ("Enter Second Number:"))
+
+sum_result = num1+num2
+
+print ("The sum of two numbers are:", sum_result)
