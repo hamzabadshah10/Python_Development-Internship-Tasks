ac14d833fb193371897a9b77b4ab725f7b984727
Hamza Badshah <154834796+hamzabadshah10@users.noreply.github.com>
2025-07-27 03:01:35 -0700

    Delete Virtual Enviroment directory

--- Virtual Enviroment/pip_list_packages/packages.py
+++
@@ -1,8 +0,0 @@
-import pkg_resources
-
-installed_packages = pkg_resources.working_set
-package_list = sorted(["{}=={}".format(pkg.key, pkg.version) for pkg in installed_packages])
-
-print("Installed pip packages:")
-for package in package_list:
-    print(package)
--- Virtual Enviroment/requests_numpy (check_versions)/check_versions.py
+++
@@ -1,5 +0,0 @@
-import requests
-import numpy
-
-print("Requests version:", requests.__version__)
-print("NumPy version:", numpy.__version__)
